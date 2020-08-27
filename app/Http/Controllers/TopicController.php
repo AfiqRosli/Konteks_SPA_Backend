9f9f464ba9029cbf44384cd68c773881a247a9ba
Afiq <afiq_rosli@live.com>
2020-08-28 01:01:14 +0800

    TopicController topics() return all topic

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Topic;
 
 class TopicController extends Controller
 {
-    //
+    public function topics() {
+        return Topic::all();
+    }
 }
