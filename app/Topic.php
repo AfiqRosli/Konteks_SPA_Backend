f947bdf9edc2d478e7154ae88cae277cd10dc28c
Afiq <afiq_rosli@live.com>
2020-09-02 01:01:34 +0800

    add lessons relationship in Topic

--- app/Topic.php
+++ app/Topic.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     protected $fillable = ["title", "desc","src"];
+
+    public function lessons() {
+        return $this->hasMany('App\Lesson');
+    }
 }
