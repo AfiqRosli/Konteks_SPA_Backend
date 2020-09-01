c029a7018f9dc50dac217d5d982be2cf2e018cc8
Afiq <afiq_rosli@live.com>
2020-09-02 01:03:10 +0800

    CreateLessonsTable migration init

---
+++ database/migrations/2020_09_01_101722_create_lessons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('topic_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('type');
+            $table->string('opening');
+            $table->string('title');
+            $table->string('context');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
