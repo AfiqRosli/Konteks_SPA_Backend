c6e6649af36461c4afe1be795a52207decb09b18
Afiq <afiq_rosli@live.com>
2020-09-02 01:01:17 +0800

    Lesson model init

---
+++ app/Lesson.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    protected $fillable = [
+        "topic_id",
+        "type",
+        "opening",
+        "title",
+        "context"
+    ];
+
+    public function topic() {
+        return $this->belongsTo('App\Topic');
+    }
+}
