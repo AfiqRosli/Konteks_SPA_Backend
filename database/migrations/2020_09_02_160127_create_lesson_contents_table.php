d92c69e6d7719ce0a1fa1634081410855c49b695
Afiq <afiq_rosli@live.com>
2020-09-03 09:05:03 +0800

    CreateLessonContentsTable init

---
+++ database/migrations/2020_09_02_160127_create_lesson_contents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesson_contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lesson_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('order')->index();
+            $table->string('name');
+            $table->string('lang');
+            $table->string('lang_translation');
+            $table->string('says');
+            $table->string('says_translation')->nullable();
+            $table->boolean('user_input');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesson_contents');
+    }
+}
